35d031c000c7136308cbc63819e21092a1195891
sivabalan <sivabalanavis@gmail.com>
2026-04-18 11:12:59 +0530

    latest changes in backend updated

---
+++ src/app.js
@@ -0,0 +1,25 @@
+// src/app.js - Express app setup
+const express = require("express");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// API Docs
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/api-docs.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
+// Routes
+app.use("/api/contact", require("./routes/contactRoutes"));
+app.use("/api/volunteer", require("./routes/volunteerRoutes"));
+app.use("/api/donation", require("./routes/donationRoutes"));
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+// src/index.js - Server entry point
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
+
+// Connect to database
+connectDB();
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
